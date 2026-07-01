781ce0cce9e5daf42d1fc0e8e1faad3c6308500f
YounJae-H <lama0812n@gmail.com>
2026-07-02 04:04:20 +0900

    fix: add bootstrap entrypoint stubs

---
+++ src/client/java/com/younjaeh/kindmap/KindMapClient.java
@@ -0,0 +1,9 @@
+package com.younjaeh.kindmap;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class KindMapClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
